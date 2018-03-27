6d4c869aca2e7463c10d2cd6f52a9274dd5f1490
Alena Prideina <alena.prideina@yandex.ru>
2018-03-27 12:50:41 +0700

    My first program on Objective-C in XCode

---
+++ prog1/prog1/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  prog1
+//
+//  Created by Alena Prideina on 27.03.18.
+//  Copyright © 2018 Prideina Alena. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+// First program example
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Programming is fun!");
+    }
+    return 0;
+}
