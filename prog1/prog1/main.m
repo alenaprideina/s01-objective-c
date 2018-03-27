3e099ba1a07f7a4864d922c43013d8324278bce1
Alena Prideina <alena.prideina@yandex.ru>
2018-03-27 21:14:35 +0700

    Chapter 2. Simple examples

--- prog1/prog1/main.m
+++ prog1/prog1/main.m
@@ -13,7 +13,18 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Programming is fun!");
+        NSLog(@"Programming is fun!\nProgramming in Objective-C is even more fun!");
+        
+        int sum, value1, value2;
+        
+        value1 = 50;
+        value2 = 25;
+        
+        sum = value1 + value2;
+        
+        NSLog(@"The sum of %i and %i is %i", value1, value2, sum);
+        
+        NSLog(@"In Objective-C, lowercase letters are significant.\nmain is where program execution begins.\nOpen and closed braces enclose program statements in a routine.\nAll program statements must be terminated by a semicolon.");
     }
     return 0;
 }
